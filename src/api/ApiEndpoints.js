44e4d68ed906cf30a84fae97324944688f52d1c5
shanuDhanusha <shanudhanusha@gmail.com>
2023-09-14 19:59:50 +0530

    changes

--- src/api/ApiEndpoints.js
+++ src/api/ApiEndpoints.js
@@ -1,2 +1,6 @@
 export const SIGNIN_URL = '/api/signinuser';
 export const SIGNUP_URL = '/api/registeruser';
+export const ADD_MEDICINE_URL = '/api/addmedicine';
+export const ADD_APPOINMENT_URL = '/api/addappoinment';
+export const ADD_STAFF_URL = '/api/addstaff';
+export const ADD_DOCTOR_URL = '/api/adddoctor';
